9acf55ea35402178eedd4333f0c83dad1ab0f386
10957393 <103188479+JEREMIAH-LAVOE@users.noreply.github.com>
2024-06-02 17:39:30 +0000

    TicketPriceCalc Files Upload

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MovieTicketCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age:");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int age))
+            {
+                if (age >= 65 || age <= 12)
+                {
+                    Console.WriteLine("Your ticket price is GHC7.");
+                }
+                else
+                {
+                    Console.WriteLine("Your ticket price is GHC10.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
+    }
+}
